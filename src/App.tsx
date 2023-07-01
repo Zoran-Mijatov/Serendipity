f4cc33cec3e31a9256a3000456fce6e2098679b1
ZoranMijatov <zoran.mijatov@bridgewaterlabs.com>
2023-07-02 00:26:47 +0200

    Adding Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Think lightly of yourself and deeply of the world</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Think lightly of yourself and deeply of the world
+    </h1>
+  );
 }
 
 export default App;
